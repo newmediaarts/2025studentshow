bfdc414c6d0e12d8ddbfa100fa5a6abfda4aee0c
Defffy <d@defffy.com>
2025-05-09 12:46:09 -1000

    feat: add student data files

---
+++ test11-website/data/animators.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    name: 'Jimmy',
+    lastName: 'Neutron',
+    url: '/animator-1',
+    image: 'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60',
+    hoverImage: 'https://picsum.photos/id/237/200/300', 
+  },
+  {
+    name: 'Abe',
+    lastName: 'Simpson',
+    url: '/animator-2',
+    image: 'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60',
+    hoverImage: 'https://picsum.photos/id/237/200/300', 
+  }
+]
+
+
---
+++ test11-website/data/interface.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: 'Benjamin',
+    lastName: 'Franklin',
+    url: '/student-1',
+    image: 'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60',
+    hoverImage: 'https://picsum.photos/id/237/200/300', 
+  },
+  {
+    name: 'Alex',
+    lastName: 'Smith',
+    url: '/student-2',
+    image: 'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60',
+    hoverImage: 'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60',
+    hoverImage: 'https://picsum.photos/id/237/200/300', 
+  },
+  {
+    name: 'Gabby',
+    lastName: 'Abby',
+    url: '/gabby',
+    hoverImage: 'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60',
+    hoverImage: 'https://picsum.photos/id/237/200/300', 
+  }
+]
+
+
