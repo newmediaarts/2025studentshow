9032c7bf224ebdc4d45f33fe7d5d6d0ccc563d99
Defffy <d@defffy.com>
2025-05-09 13:56:38 -1000

    feat: add real data

--- test11-website/data/interface.js
+++ test11-website/data/interface.js
@@ -18,8 +18,37 @@ export default [
     name: 'Gabby',
     lastName: 'Abby',
     url: '/gabby',
-    hoverImage: 'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60',
-    hoverImage: 'https://picsum.photos/id/237/200/300', 
+    image: 'https://nma.kapiolani.hawaii.edu/2025studentshow/img/gabby-abbey-studentimage.webp',
+    hoverImage: 'https://nma.kapiolani.hawaii.edu/2025studentshow/img/gabby-abbey-studentimage-hover.webp',
+    media: [
+      {
+        src: 'https://nma.kapiolani.hawaii.edu/2025studentshow/img/gabby-abbey-project1.webp',
+        alt: 'Project 1',
+        type: 'image',
+      },
+      {
+        src: 'https://nma.kapiolani.hawaii.edu/2025studentshow/img/gabby-abbey-project2.webp',
+        alt: 'Project 2',
+        type: 'image',
+      },
+      {
+        src: 'https://nma.kapiolani.hawaii.edu/2025studentshow/img/gabby-abbey-project3.webm',
+        type: 'video',
+        alt: 'Gabby Abbey Video',
+      },
+    ],
+    portfolio: 'https://www.gabbyabbey.com/',
+    social: [
+      {
+        label: 'linkedin',
+        url: 'https://www.linkedin.com/in/gabriellaabbey/'
+      },
+      {
+        label: 'instagram',
+        url: 'https://www.instagram.com/msgabbyabbey'
+      }
+
+    ]
   }
 ]
 
