d796f1268da47a1ab7b63b861e09248f576a6c09
lummelis <lummelis@hawaii.edu>
2025-05-12 12:46:30 -1000

    init v14

---
+++ test14-website/data/animators.js
@@ -0,0 +1,287 @@
+export default [
+  {
+    name: 'Maya',
+    lastName: 'Bautista',
+    url: 'maya',
+    image: 'img/portrait-maya.webp',
+    hoverImage: 'img/portrait-maya-hover.webp', 
+    media: [
+      {
+        src: 'img/maya-bautista-project1.webm',
+        alt: 'Project 1',
+        type: 'video',
+      },
+      {
+        src: 'img/maya-bautista-project2.webm',
+        alt: 'Project 2',
+        type: 'video',
+      },
+      {
+        src: 'img/maya-bautista-project3.webm',
+        alt: 'Project 3',
+        type: 'video',
+      },
+      {
+        src: 'img/maya-bautista-project4.webp',
+        alt: 'Project 4',
+        type: 'image',
+      },
+      {
+        src: 'img/maya-bautista-project5.webm',
+        alt: 'Project 5',
+        type: 'video',
+      },
+      {
+        src: 'img/maya-bautista-project6.webm',
+        alt: 'Project 6',
+        type: 'video',
+      },
+    ],
+    portfolio: 'https://maize.cargo.site/',
+    social: [
+      {
+        label: 'instagram',
+        url: 'https://www.instagram.com/mirrormaize/'
+      }
+
+    ]
+  },
+  {
+    name: 'Rheddie James',
+    lastName: 'Acosta',
+    url: 'rheddie',
+    image: 'img/portrait-rheddie.webp',
+    hoverImage: 'img/portrait-rheddie-hover.webp', 
+    media: [
+      {
+        src: 'img/RheddieAcosta-Project1.webm',
+        alt: 'Project 1',
+        type: 'video',
+      },
+      {
+        src: 'img/RheddieAcosta-Project2.webp',
+        alt: 'Project 2',
+        type: 'video',
+      },
+      {
+        src: 'img/RheddieAcosta-Project3.webp',
+        alt: 'Project 3',
+        type: 'image',
+      },
+      {
+        src: 'img/RheddieAcosta-Project3.webm',
+        alt: 'Demo Reel',
+        type: 'video',
+      },
+    ],
+    portfolio: 'https://rheddie.wixsite.com/nmaportfolio',
+  },
+  {
+    name: 'Noelani',
+    lastName: 'Fortin',
+    url: 'noelani',
+    image: 'img/portrait-noelani.webp',
+    hoverImage: 'img/portrait-noelani-hover.webp', 
+    media: [
+      {
+        src: 'img/Noelani-Fortin-Project1.webm',
+        alt: 'Project 1',
+        type: 'video',
+      },
+      {
+        src: 'img/Noelani-Fortin-Project2.webm',
+        alt: 'Project 2',
+        type: 'video',
+      },
+      {
+        src: 'img/Noelani-Fortin-Project3.webp',
+        alt: 'Project 3',
+        type: 'image',
+      },
+      {
+        src: 'img/Noelani-Fortin-demoreel.webm',
+        alt: 'Demo Reel',
+        type: 'video',
+      },
+    ],
+    portfolio: 'https://fortinnl.wixsite.com/noelaniforitn',
+  },
+  {
+    name: 'Lucas',
+    lastName: 'Stevens',
+    url: 'lucas',
+    image: 'img/portrait-lucas.webp',
+    hoverImage: 'img/portrait-lucas-hover.webp', 
+    media: [
+      {
+        src: 'img/Lucas-Stevens-Project1.webm',
+        alt: 'Project 1',
+        type: 'video',
+      },
+      {
+        src: 'img/Lucas-Stevens-Project2.webm',
+        alt: 'Project 2',
+        type: 'video',
+      },
+      {
+        src: 'img/Lucas-Stevens-Project3.webm',
+        alt: 'Project 3',
+        type: 'video',
+      },
+      {
+        src: 'img/Lucas-Stevens-Project4.webm',
+        alt: 'Project 4',
+        type: 'video',
+      },
+    ],
+    portfolio: 'https://www.lucasstevens.art/',
+  },
+  {
+    name: 'Austin',
+    lastName: 'Ubogen',
+    url: 'austin',
+    image: 'img/portrait-austin.webp',
+    hoverImage: 'img/portrait-austin-hover.webp', 
+    media: [
+      {
+        src: 'img/austin-ubongen-Project1.webm',
+        alt: 'Project 1',
+        type: 'video',
+      },
+      {
+        src: 'img/austin-ubongen-Project2.webm',
+        alt: 'Project 2',
+        type: 'video',
+      },
+      {
+        src: 'img/austin-ubongen-Project3.webp',
+        alt: 'Project 3',
+        type: 'image',
+      },
+    ],
+  },
+  {
+    name: 'Thomas',
+    lastName: 'Drown',
+    url: 'thomas',
+    image: 'img/portrait-thomas.webp',
+    hoverImage: 'img/portrait-thomas-hover.webp', 
+    media: [
+      {
+        src: 'img/thomas-drown-Project1.webp',
+        alt: 'Project 1',
+        type: 'image',
+      },
+      {
+        src: 'img/thomas-drown-Project2.webm',
+        alt: 'Project 2',
+        type: 'video',
+      },
+      {
+        src: 'img/thomas-drown-Project3.webm',
+        alt: 'Project 3',
+        type: 'video',
+      },
+    ],
+    portfolio: 'https://thomaskdrown.wixsite.com/my-art-portfolio',
+  },
+  {
+    name: 'Mekiah',
+    lastName: 'Tong',
+    url: 'mekiah',
+    image: 'img/portrait-mekiah.webp',
+    hoverImage: 'img/portrait-mekiah-hover.webp', 
+    media: [
+      {
+        src: 'img/MekiahTong-Project1.webm',
+        alt: 'Project 1',
+        type: 'video',
+      },
+      {
+        src: 'img/MekiahTong-Project2.webm',
+        alt: 'Project 2',
+        type: 'video',
+      },
+      {
+        src: 'img/MekiahTong-Project3.webm',
+        alt: 'Project 3',
+        type: 'video',
+      },
+    ],
+  },
+  {
+    name: 'Race',
+    lastName: 'Nacapuy',
+    url: 'race',
+    image: 'img/portrait-race.webp',
+    hoverImage: 'img/portrait-race-hover.webp', 
+    media: [
+      {
+        src: 'img/Race-Nacapuy-Project1.webp',
+        alt: 'Project 1',
+        type: 'image',
+      },
+      {
+        src: 'img/Race-Nacapuy-Project2.webp',
+        alt: 'Project 2',
+        type: 'image',
+      },
+      {
+        src: 'img/Race-Nacapuy-Project3.webm',
+        alt: 'Project 3',
+        type: 'video',
+      },
+      {
+        src: 'img/Race-Nacapuy-demoreel.webm',
+        alt: 'DemoReel',
+        type: 'video',
+      },
+    ],
+    portfolio: 'https://nacapuyr.wixsite.com/nma-portfolio',
+  },
+  {
+    name: 'Zhuo Zheng',
+    lastName: 'Zheng',
+    url: 'philip',
+    image: 'img/portrait-philip.webp',
+    hoverImage: 'img/portrait-philip-hover.webp', 
+    media: [
+      {
+        src: 'img/zhuo-zheng-zheng-Project1.webm',
+        alt: 'Project 1',
+        type: 'video',
+      },
+      {
+        src: 'img/zhuo-zheng-zheng-Project2.webp',
+        alt: 'Project 2',
+        type: 'image',
+      },
+    ],
+  },
+  {
+    name: 'Richard "Esco"',
+    lastName: 'Escobar-Mo',
+    url: 'esco',
+    image: 'img/portrait-esco.webp',
+    hoverImage: 'img/portrait-esco-hover.webp', 
+    media: [
+      {
+        src: 'img/richard-escobar-mo-project1.webp',
+        alt: 'Project 1',
+        type: 'image',
+      },
+      {
+        src: 'img/richard-escobar-mo-project2.webm',
+        alt: 'Project 2',
+        type: 'video',
+      },
+      {
+        src: 'img/richard-escobar-mo-project3.webm',
+        alt: 'Project 3',
+        type: 'video',
+      },
+    ],
+  }
+]
+
+
